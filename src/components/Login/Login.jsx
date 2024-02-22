7b9e74ad9b8b50193589f18762437ceeae2d500f
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-02-22 16:10:28 -0300

    Validate token

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import LoginCreate from "./LoginCreate";
 import LoginPasswordLost from "./LoginPasswordLost";
 import LoginPasswordReset from "./LoginPasswordReset";
+import { UserContext } from "../../UserContext";
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+  const navigate = useNavigate();
+
+  if (login === true) {
+    navigate("/conta");
+  }
+
   return (
     <Routes>
       <Route path="/" element={<LoginForm />} />
