c32d26d4fd7510fda2a012cb3da4cc505f48b969
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-02-29 11:55:47 -0300

    Perfil usuário

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { UserStorage } from "./UserContext";
 import User from "./components/User/User";
 import ProtectedRoute from "./components/Helper/ProtectedRoute";
 import Photo from "./components/Photo/Photo";
+import UserProfile from "./components/User/UserProfile";
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
